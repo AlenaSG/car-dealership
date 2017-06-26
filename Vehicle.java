80c52b35c30e1f106c423864b045265e829c1c53
Alena Golovina and Joel Bakken <joel@>
2017-06-26 15:46:24 -0700

    add constructors to the vehicle file

--- Vehicle.java
+++ Vehicle.java
@@ -1,11 +1,22 @@
+
 class Vehicle {
+
   public int mYear;
   public String mBrand;
   public String mModel;
   public int mMiles;
   public int mPrice;
 
-  public boolean worthBuying(int maxPrice) {
+  public Vehicle(int year, String brand, String model, int miles, int price) {
+    mYear = year;
+    mBrand = brand;
+    mModel = model;
+    mMiles = miles;
+    mPrice = price;
+  }
+
+  public boolean worthBuying(int maxPrice){
     return (mPrice < maxPrice);
   }
+
 }
